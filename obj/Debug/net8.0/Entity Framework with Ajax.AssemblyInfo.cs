24de9e1d0de284527363b56e79634e5b5ef20446
unknown <zeeshannoorofficial@gmail.com>
2024-05-07 18:17:25 +0500

    DataTable Apply

--- obj/Debug/net8.0/Entity Framework with Ajax.AssemblyInfo.cs
+++ obj/Debug/net8.0/Entity Framework with Ajax.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Entity Framework with Ajax")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+30f5629a1e7c9a7a1af8dad1ffd1a2a846d4e5b0")]
 [assembly: System.Reflection.AssemblyProductAttribute("Entity Framework with Ajax")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Entity Framework with Ajax")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
